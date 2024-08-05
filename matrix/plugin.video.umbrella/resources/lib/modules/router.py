7a576ea3c38ce8dff414cd5c6eb8127bde4b15cf
umbrellaplug <bobmaroney929@gmail.com>
2024-08-05 10:26:37 -0500

    workaround

--- matrix/plugin.video.umbrella/resources/lib/modules/router.py
+++ matrix/plugin.video.umbrella/resources/lib/modules/router.py
@@ -14,9 +14,10 @@ def router(argv2):
 			argv4 = xbmc.getInfoLabel('ListItem.FileNameAndPath')
 			argv5 = argv4.split('plugin://plugin.video.umbrella/?')[1]
 			params = dict(parse_qsl(argv5.replace('?', '')))
-			if params.get('action') == 'play_Item' and params.get('mediatype') == 'episode':
+			episode = params.get('episode')
+			if params.get('action') == 'play_Item' and episode:
 				from resources.lib.modules import log_utils
-				log_utils.log('Umbrella hacky workaround for blank paths being used.', 1)
+				log_utils.log('Umbrella Hacky Workaround is being applied. Thanks Adam', 1)
 				argv4 = argv4.split('plugin://plugin.video.umbrella/?')[1]
 				argv2 = argv4
 	except:
