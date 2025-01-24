789539f3b4534e3c1404985d2d3999cbebd30719
umbrellaplug <bend49571@gmail.com>
2025-01-23 18:12:12 -0600

    torbox

--- matrix/plugin.video.umbrella/resources/lib/debrid/torbox.py
+++ matrix/plugin.video.umbrella/resources/lib/debrid/torbox.py
@@ -136,7 +136,6 @@ class TorBox:
 				selected_files.sort(key=lambda k: k['size'], reverse=True)
 			if not selected_files: return None
 			file_key = selected_files[0]['link']
-			log_utils.log('TorBox: Unrestricted Link: "%s"' % magnet_url)
 			file_url = self.unrestrict_link(file_key)
 			
 			if not self.store_to_cloud: Thread(target=self.delete_torrent, args=(torrent_id,)).start()
