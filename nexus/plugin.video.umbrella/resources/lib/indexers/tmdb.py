6f3f32fa113f435862d3512d0d49169976462e0c
umbrellakodi <bend49571@gmail.com>
2025-10-03 11:07:10 -0500

    tvshow fix

--- nexus/plugin.video.umbrella/resources/lib/indexers/tmdb.py
+++ nexus/plugin.video.umbrella/resources/lib/indexers/tmdb.py
@@ -1218,7 +1218,7 @@ class TVshows(TMDb):
 		if artworkType == 'poster':
 			tmdbart_items = [item for item in tmdbart.get('posters', []) if (item.get('iso_639_1') == self.lang or item.get('iso_639_1') == None)]
 		elif artworkType == 'fanart':
-			tmdbart_items = [item for item in tmdbart.get('backdrops', []) if (item.get('iso_639_1') == self.lang or item.get('iso_639_1') == None)]
+			tmdbart_items = [item for item in tmdbart.get('backdrops', []) if (item.get('iso_639_1') == self.lang or item.get('iso_639_1') == 'xx')]
 		elif artworkType == 'landscape':
 			tmdbart_items = [item for item in tmdbart.get('backdrops', []) if (item.get('iso_639_1') == self.lang or item.get('iso_639_1') == None)]
 		elif artworkType == 'clearlogo':
